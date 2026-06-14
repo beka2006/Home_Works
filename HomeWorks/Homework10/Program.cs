6882430d785db86e71fd977c62e532dd9ee2cb49
Beka Gorgiladze <gorgiladze.beka@kiu.edu.ge>
2026-06-14 21:51:31 +0400

    Homework10 (#7)
    * added updates for review
    
    * Added Homewoork 5
    
    * Add Homework 6
    
    * Fix code HW6
    
    * Add Homework7 solutions
    
    * Fix: Add .gitignore and remove bin/obj folders
    
    * solutions of HW8
    
    * homework 9 solutions
    
    * Add homework 10 solutions
    
    * Remove build artifacts and update gitignore

---
+++ HomeWorks/Homework10/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+// N1 ამოცანა 
+namespace HomeWorks
+{
+    abstract class FileWorker
+    {
+        private int MaxStorage;
+        protected abstract string FilePath { get; set; }
+
+        public FileWorker(int maxStorage)
+        {
+            MaxStorage = maxStorage;
+        }
+
+        public virtual string Read()
+        {
+            if (!File.Exists(FilePath))
+            {
+                return "File not found";
+            }
+
+            string content = File.ReadAllText(FilePath);
+            Console.WriteLine($"I can read file with max storage {MaxStorage}");
+            return content;
+        }
+
+        public virtual void Write(string content)
+        {
+            if (content.Length > MaxStorage)
+            {
+                Console.WriteLine("Storage is full");
+                return;
+            }
+            File.WriteAllText(FilePath, content);
+            Console.WriteLine($"I Can write to txt file with max storage {MaxStorage}");
+        }
+
+        public void Edit(string newContent)
+        {
+            if (newContent.Length > MaxStorage)
+            {
+                Console.WriteLine("Content exceeds max storage limit.");
+                return;
+            }
+            File.WriteAllText(FilePath, newContent);
+            Console.WriteLine($"I can edit file with max storage {MaxStorage}");
+        }
+
+        public void Delete()
+        {
+            if (File.Exists(FilePath))
+            {
+                File.Delete(FilePath);
+                Console.WriteLine($"I can delete file {FilePath}");
+            }
+        }
+    }
+
+    class TextFileWorker : FileWorker
+    {
+        protected override string FilePath { get; set; }
+
+        public TextFileWorker(string filePath, int maxStorage) : base(maxStorage)
+        {
+            FilePath = filePath;
+        }
+    }
+}
